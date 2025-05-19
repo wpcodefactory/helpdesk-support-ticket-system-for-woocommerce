6ca8649e50c93c5c34483026e6279aa5cc24217a
algoritmika <info@algoritmika.com>
2025-05-19 15:56:55 +0300

    Fix settings

--- includes.php
+++ includes.php
@@ -2,6 +2,8 @@
 /**
  * Helpdesk Support Ticket System for WooCommerce - STSWooCommerceInc Class
  *
+ * @version 2.0.0
+ *
  * @author  WPFactory
  */
 
@@ -11,7 +13,7 @@ class STSWooCommerceInc {
 
 	public $plugin = 'STSWooCommerce';
 	public $name = 'Support Ticket System for WooCommerce';
-	public $slug = 'support-ticket-system-for-woocommerce';
+	public $slug = 'support-ticket-system-woocommerce';
 	public $tableName = 'stsw_responses';
 	public $stswpro_table_db_version = '1.4';
 	public $mailIt_allowed_html = array(
